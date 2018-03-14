2d97be8154bd1cfefa70ad5213e39dc96e2d1e88
Mukesh purohit <mpurohit88@live.com>
2018-03-15 01:26:05 +0530

    cross origin support added

--- Server.js
+++ Server.js
@@ -9,39 +9,46 @@ var smtpTransport = nodemailer.createTransport({
     service: "gmail",
     host: "smtp.gmail.com",
     auth: {
-        user: "finefitting05@gmail.com",
-        pass: "Finefitting04@123"
+        user: "finefitting04@gmail.com",
+        pass: "Finefitting@123"
     }
 });
 /*------------------SMTP Over-----------------------------*/
 
 /*------------------Routing Started ------------------------*/
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/',function(req,res){
   res.end("ok");
 });
 
 app.get('/send',function(req,res){
-    var mailOptions={
-        to : req.query.to,
-        subject : req.query.subject,
-        text : req.query.text
+  let body = "Message: " + req.query.text + " Email Address: " + req.query.to;
+
+  const mailOptions={
+      to : "mpurohit88@gmail.com",
+      subject : req.query.subject,
+      text : body
+  }
+  console.log(mailOptions);
+  smtpTransport.sendMail(mailOptions, function(error, response){
+    if(error){
+      console.log(error);
+      res.end("error");
+    }else{
+      console.log("Message sent: " + response.message);
+      res.end("sent");
     }
-    console.log(mailOptions);
-    smtpTransport.sendMail(mailOptions, function(error, response){
-     if(error){
-            console.log(error);
-        res.end("error");
-     }else{
-            console.log("Message sent: " + response.message);
-        res.end("sent");
-         }
   });
 });
 
 /*--------------------Routing Over----------------------------*/
 
-app.listen(process.env.PORT || 3000,function(){
+app.listen(process.env.PORT || 3001,function(){
     const port = process.env.PORT || 3000;
     console.log("Express Started on Port " + port);
 });
